c4371c07fccc5d2a5af7c6a2656fd76a0e2d1290
unsafeact <un@safe.act>
2018-04-27 22:46:14 +0200

    4.2 Added prices to products, defaults for empty values, ready to submit

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_04_27_191459) do
+ActiveRecord::Schema.define(version: 2018_04_27_195938) do
 
   create_table "products", force: :cascade do |t|
     t.string "name"
@@ -18,7 +18,8 @@ ActiveRecord::Schema.define(version: 2018_04_27_191459) do
     t.string "image_url"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.string "color"
+    t.string "colour"
+    t.decimal "price"
   end
 
 end
