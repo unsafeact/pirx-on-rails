df6bbf92ad199d9d324a6ec7ff841be09122b9dc
unsafeact <un@safe.act>
2018-05-07 23:42:13 +0200

    5.1 Routes

--- app/controllers/simple_pages_controller.rb
+++ app/controllers/simple_pages_controller.rb
@@ -7,4 +7,13 @@ class SimplePagesController < ApplicationController
     @products = Product.limit(4)
   end
 
+  def before_landing
+    render layout: "landing_layout"
+  end
+
+  def contact
+    # sleep 1
+    redirect_to root_path, notice: "Sorry, no contact! You have been redirected!"
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,8 @@ Rails.application.routes.draw do
   get 'simple_pages/about'
   get 'simple_pages/contact'
   get 'simple_pages/index'
+  get 'simple_pages/landing_page'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root 'simple_pages#landing_page'
+  root 'simple_pages#before_landing'
 end
