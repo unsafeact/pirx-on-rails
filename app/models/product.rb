e3ae26a7179b6d0d83307dab0bae6471ea98d304
unsafeact <un@safe.act>
2018-05-17 20:14:12 +0200

    5.7 Comment model

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,6 +1,7 @@
 class Product < ApplicationRecord
 
 has_many :orders
+has_many :comments
 
   def self.search(search_term)
     if Rails.env.development? then
@@ -8,6 +9,14 @@ has_many :orders
     else
       Product.where("name ilike ?", "%#{search_term}%")
     end
+  end
+
+  def highest_rating_comment
+    comments.rating_desc.first
+  end
+
+  def lowest_rating_comment
+    comments.rating_desc.last
   end
 
 end
