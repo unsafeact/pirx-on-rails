f2f25a2decd0a459beea37d08ad7ea49799057f0
unsafeact <un@safe.act>
2018-05-18 20:29:15 +0200

    5.8 changed image paths for raty

--- app/assets/javascripts/site.js
+++ app/assets/javascripts/site.js
@@ -1,17 +1,17 @@
 $(document).on('turbolinks:load', function(){
     $('.rating').raty({
-      path: '/assets/',
-      // starHalf: '<%= asset_path 'star-half.png' %>',
-      // starOff: '<%= asset_path 'star-off.png' %>',
-      // starOn: '<%= asset_path 'star-on.png' %>',
+      // path: '/assets/',
+      starHalf: '<%= asset_path 'star-half.png' %>',
+      starOff: '<%= asset_path 'star-off.png' %>',
+      starOn: '<%= asset_path 'star-on.png' %>',
       scoreName: 'comment[rating]'
     });
 
     $('.rated').raty({
-      path: '/assets/',
-      // starHalf: '<%= asset_path 'star-half.png' %>',
-      // starOff: '<%= asset_path 'star-off.png' %>',
-      // starOn: '<%= asset_path 'star-on.png' %>',
+      // path: '/assets/',
+      starHalf: '<%= asset_path 'star-half.png' %>',
+      starOff: '<%= asset_path 'star-off.png' %>',
+      starOn: '<%= asset_path 'star-on.png' %>',
       readOnly: true,
       score: function() {
         return $(this).attr('data-score');
