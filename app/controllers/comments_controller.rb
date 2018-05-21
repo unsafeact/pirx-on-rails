626a8f5c6d65a78c284309de0e53109e8d7ad197
unsafeact <un@safe.act>
2018-05-21 17:23:24 +0200

    removed stupid # from comments_controller

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -25,7 +25,7 @@ class CommentsController < ApplicationController
   private
 
   def comment_params
-    # params.require(:comment).permit(:user_id, :body, :rating)
+    params.require(:comment).permit(:user_id, :body, :rating)
   end
 
 end
