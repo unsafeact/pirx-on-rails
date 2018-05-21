931664c2978ba62e153ff09ef8f7d76ac7278a2c
unsafeact <un@safe.act>
2018-05-21 19:55:09 +0200

    fixed authorization for destrying comments

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -5,7 +5,7 @@ class Ability
     user ||= User.new # guest user (not logged in)
     can :manage, User, id: user.id
     can :manage, Order, user_id: user.id
-    can :destroy, Comment, admin: true
+    can :destroy, Comment if user.admin?
   end
 
 end
