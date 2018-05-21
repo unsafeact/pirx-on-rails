cd11c84a91a2edcd64323a2e285ba429532f6a4f
unsafeact <un@safe.act>
2018-05-21 15:57:14 +0200

    5.10 Admin role & Moderating user comments

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -16,12 +16,16 @@ class CommentsController < ApplicationController
     end
 
   def destroy
+    @comment = Comment.find(params[:id])
+    product = @comment.product
+    @comment.destroy
+    redirect_to product
   end
 
   private
 
   def comment_params
-    params.require(:comment).permit(:user_id, :body, :rating)
+    # params.require(:comment).permit(:user_id, :body, :rating)
   end
 
 end
--- app/models/ability.rb
+++ app/models/ability.rb
@@ -5,6 +5,7 @@ class Ability
     user ||= User.new # guest user (not logged in)
     can :manage, User, id: user.id
     can :manage, Order, user_id: user.id
+    can :destroy, Comment, admin: true
   end
 
 end
