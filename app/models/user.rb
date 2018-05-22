0cd29aa501ea4c95dd2783f331daba65cf2f9d92
unsafeact <un@safe.act>
2018-05-22 17:24:25 +0200

    dependent:destroy on orders and comments

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,11 +3,6 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :confirmable, :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
-  has_many :orders
-  has_many :comments
-
-  # after_create :send_welcome_mail
-
-
-
+  has_many :orders, dependent: :destroy
+  has_many :comments, dependent: :destroy
 end
