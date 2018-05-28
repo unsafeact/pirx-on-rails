25c59b1cb7a51aaf825821e28cb2c6d374b0bd0c
unsafeact <un@safe.act>
2018-05-28 23:10:11 +0200

    6.6 Creating payments

---
+++ app/controllers/payments_controller.rb
@@ -0,0 +1,37 @@
+class PaymentsController < ApplicationController
+
+  before_action :authenticate_user!
+
+  def create
+    token = params[:stripeToken]
+    @product = Product.find(params[:hidden_product_id].to_i)
+    @user = current_user
+
+    # Create the charge on Stripe's servers - this will charge the user's card
+    begin
+      charge = Stripe::Charge.create(
+        amount: (@product.price * 100).to_i, # amount in cents, again
+        currency: "eur",
+        source: token,
+        description: params[:stripeEmail] # according to Course material
+        # "receipt_email: 'jenny.rosen@example.com'" example from Stripe docs to override the default customer email address
+        # receipt_email: params[:stripeEmail]
+      )
+
+      if charge.paid
+        Order.create(user_id: @user, product_id: @product.id, total: @product.price)
+      end
+
+    rescue Stripe::CardError => e
+      # The card has been declined
+      body = e.json_body
+      err = body[:error]
+      flash[:error] = "Unfortunately, there was an error processing your payment: #{err[:message]}"
+    end
+
+    redirect_to product_path(@product)
+    flash[:notice] = "Your order has been placed!"
+
+  end
+
+end
