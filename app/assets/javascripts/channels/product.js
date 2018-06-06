cdb2f74373efce6b8ff9ce226ba4074023256734
unsafeact <un@safe.act>
2018-06-06 15:16:00 +0200

    6.14 ActiveJob and Advanced ActionCable 1st part

--- app/assets/javascripts/channels/product.js
+++ app/assets/javascripts/channels/product.js
@@ -7,8 +7,19 @@ App.product = App.cable.subscriptions.create("ProductChannel", {
     // Called when the subscription has been terminated by the server
   },
 
-  received: function(data) {
+  received: function() {
     // Called when there's incoming data on the websocket for this channel
     $(".alert.alert-info").show();
-  }
+  },
+
+  listen_to_comments: function() {
+    return this.perform('listen', {
+      product_id: $("[data-product-id]").data("product-id")
+    });
+  },
+
+});
+
+$(document).on('turbolinks:load', function() {
+  App.product.listen_to_comments();
 });
