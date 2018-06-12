d2fb8824b8e8f9b478dc292c3566ee1bdd6159f1
unsafeact <un@safe.act>
2018-06-12 09:03:05 +0200

    6.15 fixed ability for default user, changed user/edit user/show views

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -7,11 +7,9 @@ class Ability
     can :manage, Order, user_id: user.id
 
     if user.admin?
-      can :manage, User
-      can :manage, Product
-      can :destroy, Comment
+      can :manage, all
     else
-      can [:edit, :update, :destroy], User, id: user.id
+      can [:read, :edit, :update, :destroy], User, id: user.id
       can :read, Product
       can :manage, Order, user_id: user.id
     end
